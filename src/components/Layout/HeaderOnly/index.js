88608c5b9ffc0ec232a7fccbeaeb301375ef3880
Minhtu203 <minhtu09042003@gmail.com>
2025-07-05 10:50:01 +0700

    10h46 5/7

---
+++ src/components/Layout/HeaderOnly/index.js
@@ -0,0 +1,9 @@
+import Header from './Header'
+
+export default function HeaderOnly() {
+  return (
+    <>
+      <div><Header /></div>
+    </>
+  )
+}
---
+++ src/pages/Upload/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../../components/Layout/HeaderOnly'
+
+export default function Upload() {
+  return (
+    <div>
+      <h2>Upload Page</h2>
+      <Header />
+    </div>
+  )
+}
